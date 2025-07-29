c39a2e880aaa9e7cf27e2cfd05d62f56ed8d30d3
Vincci-ZeeChing <vincci-zeeching@outlook.com>
2025-07-29 22:53:30 +0800

    :art: Adjust responsive

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,35 +1,55 @@
 import './About.css'
 
 function About() {
-  return (
-    <div>
-      <div className="aboutPage">
-        <p className='expert'>Your Expert in </p>
-        <p className='textCard'>Frontend Development & Tag Implementation</p>
-        <p className='content'>
-          With hands-on experience across China, Taiwan, and Hong Kong,
-          I build high-performance websites and deliver precise tracking solutions
-          through Google Ads, GA4, and GTM—empowering brands to optimize, track,
-          and grow with confidence.
-        </p>
+  return (<div>
+    <div className="aboutPage">
 
-      </div>
-      <div className='buttonContainer'>
-        <button className='button'>Let’s work together
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            fill="currentColor"
-            style={{ marginLeft: '10px', transform: 'scaleX(-1)' }}
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 4l1.41 1.41L7.83 11H20v2H7.83l5.58 5.59L12 20l-8-8z" />
-          </svg>
-        </button>
-      </div>
+      <p className="expert">
+          <span className="y-wrapper">
+            <span className="svg-icon">
+              <svg
+                viewBox="0 0 82 63"
+                preserveAspectRatio="xMidYMid meet"
+                xmlns="http://www.w3.org/2000/svg"
+                className="svg-shape"
+              >
+                <line x1="5.55" y1="54.62" x2="29.62" y2="57.45" stroke="#1E82A6" strokeWidth="10"
+                      strokeLinecap="round"/>
+                <line x1="23.66" y1="5.36" x2="39.36" y2="38.34" stroke="#1E82A6" strokeWidth="10"
+                      strokeLinecap="round"/>
+                <line x1="76.14" y1="24.74" x2="66.74" y2="42.86" stroke="#1E82A6" strokeWidth="10"
+                      strokeLinecap="round"/>
+              </svg>
+            </span>
+            Y
+          </span>
+        our Expert in
+      </p>
+
+      <p className='textCard'>Frontend Development & Tag Implementation</p>
+      <p className='content'>
+        With hands-on experience across China, Taiwan, and Hong Kong,
+        I build high-performance websites and deliver precise tracking solutions
+        through Google Ads, GA4, and GTM—empowering brands to optimize, track,
+        and grow with confidence.
+      </p>
+
     </div>
-  )
+    <div className='buttonContainer'>
+      <button className='button'>Let’s work together
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          fill="currentColor"
+          style={{marginLeft: '10px', transform: 'scaleX(-1)'}}
+          viewBox="0 0 24 24"
+        >
+          <path d="M12 4l1.41 1.41L7.83 11H20v2H7.83l5.58 5.59L12 20l-8-8z"/>
+        </svg>
+      </button>
+    </div>
+  </div>)
 }
 
 export default About;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,7 @@
 import './LandingPage.css';
 import Homepage from "./Homepage/Homepage";
 import About from "./About/About";
+import Services from "./Services";
 
 const LandingPage = () => {
   return (
@@ -14,6 +15,9 @@ const LandingPage = () => {
         <About/>
       </section>
 
+      <section className="section">
+        <Services/>
+      </section>
       {/* Add more sections like Skills, Services, Contact here in same layout */}
     </div>
   );
